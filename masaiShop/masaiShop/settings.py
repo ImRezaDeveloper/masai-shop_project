d1d75311920d6a5cfa8df74c77a1f253092577ff
Reza <rezapapi138418@gmail.com>
2025-06-22 16:05:40 +0330

    add statics file and add postgresql database to project

--- masaiShop/masaiShop/settings.py
+++ masaiShop/masaiShop/settings.py
@@ -72,13 +72,23 @@ WSGI_APPLICATION = 'masaiShop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'masaishop_db',      # همان نام دیتابیس
+        'USER': 'reza_admin',       # نام کاربری
+        'PASSWORD': 'Reza.Papi@masai2025',  # رمز عبور
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -115,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
