db23a714e65d405fd414551586504af0eb0e50c3
Reza <rezapapi138418@gmail.com>
2025-06-24 22:43:09 +0330

    finish step one of develop product model with template

--- masaiShop/product/views.py
+++ masaiShop/product/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from .models import ProductModel
 from django.shortcuts import get_object_or_404
-
+from django.views.generic.detail import DetailView
 # Create your views here.
 
-def productView(request, slug):
-    product = get_object_or_404(ProductModel, slug=slug)
-    return render(request, 'product/single-product.html', context={'product': product})
+# def productView(request, slug):
+#     product = get_object_or_404(ProductModel, slug=slug)
+#     return render(request, 'product/single-product.html', context={'product': product})
+
+
+class ProductDetail(DetailView):
+    template_name = 'product/single-product.html'
+    model = ProductModel
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["product"] = get_object_or_404(ProductModel)
+        return context
+    
