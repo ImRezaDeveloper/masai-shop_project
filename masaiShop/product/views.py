4f0e4b62ab9d27e98fc77603a24d362383bdde88
Reza <rezapapi138418@gmail.com>
2025-06-27 16:40:07 +0330

    merge product to main branch

--- masaiShop/product/views.py
+++ masaiShop/product/views.py
@@ -8,11 +8,15 @@ from django.views.generic import DetailView, ListView
 #     product = get_object_or_404(ProductModel, slug=slug)
 #     return render(request, 'product/single-product.html', context={'product': product})
 
-class ProductList(ListView):
+class ProductList(DetailView):
     template_name = 'product/products.html'
     model = ProductModel
 
-    context_object_name = 'products'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["products"] = get_object_or_404(ProductModel)
+        return context
+    
 class ProductDetail(DetailView):
     template_name = 'product/single-product.html'
     model = ProductModel
