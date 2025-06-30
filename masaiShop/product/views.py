4d711b5cfb361bc345777d8362821df02200b7fd
Reza <rezapapi138418@gmail.com>
2025-06-30 23:47:54 +0330

    fix category section

--- masaiShop/product/views.py
+++ masaiShop/product/views.py
@@ -19,8 +19,15 @@ class ProductList(ListView):
 class ProductDetail(DetailView):
     template_name = 'product/single-product.html'
     model = ProductModel
+    context_object_name = 'product'
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["product"] = get_object_or_404(ProductModel)
-        return context
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if 'slug' in self.kwargs:
+            queryset = queryset.filter(slug=self.kwargs['slug'])
+        return queryset
+    
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["product"] = get_object_or_404(ProductModel)
+    #     return context
